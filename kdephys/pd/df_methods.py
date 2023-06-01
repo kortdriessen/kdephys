847494be57305499040a1897b6a8eb7427db1d0c
Kort Driessene <driessenkort@gmail.com>
2023-05-31 19:12:13 -0500

    changes to df and pldf methods

--- kdephys/pd/df_methods.py
+++ kdephys/pd/df_methods.py
@@ -4,35 +4,58 @@ import numpy as np
 import pandas as pd
 import pandas_flavor as pf
 
-#----------------------------------------- for General Unit Dataframes ------------------------------------------
+# ----------------------------------------- for General Unit/Info Dataframes ------------------------------------------
+
 
 @pf.register_dataframe_method
 def xnote(self, notes=["noisy", "bw"]):
     return self.loc[~self.note.str.contains("|".join(notes))]
 
+
 @pf.register_dataframe_method
 def rec(self, rec):
     return self.loc[self.recording == rec]
+
 
 @pf.register_dataframe_method
 def stm(self):
     return self.loc[self.stim == 1]
 
-@pf.register_dataframe_method
-def prb(self, probe='NNXr'):
-    return self.loc[self.probe == probe]
 
 @pf.register_dataframe_method
-def pclus(self, probe='NNXr', cluster=0):
-    return self.loc[np.logical_and(self.probe == probe, self.cluster_id == cluster)]
+def cid(self, cid):
+    return self.loc[self["cluster_id"] == cid]
+
+
+@pf.register_dataframe_method
+def pclus(self, probe, cid):
+    return self.loc[(self["cluster_id"] == cid) & (self["probe"] == probe)]
+
+
+@pf.register_dataframe_method
+def cid_un(self):
+    return self["cluster_id"].unique()
+
+
+@pf.register_dataframe_method
+def prb(self, probe="NNXr"):
+    return self.loc[self.probe == probe]
+
+
+@pf.register_dataframe_method
+def prbs(self):
+    return self["probe"].unique()
+
 
 @pf.register_dataframe_method
 def st(self, state):
     return self.loc[self.state == state]
 
+
 @pf.register_dataframe_method
 def sid(self, sort_id):
     return self.loc[self.sort_id == sort_id]
+
 
 @pf.register_dataframe_method
 def ts(self, t1, t2):
@@ -45,24 +68,26 @@ def ts(self, t1, t2):
     if type(t1) == str:
         t1 = np.datetime64(t1)
         t2 = np.datetime64(t2)
-        if 'datetime' in self.columns:
+        if "datetime" in self.columns:
             return self.loc[np.logical_and(self.datetime >= t1, self.datetime <= t2)]
-        elif 'datetime' not in self.columns:
+        elif "datetime" not in self.columns:
             return self.loc[np.logical_and(self.index >= t1, self.index <= t2)]
     elif type(t1) == np.datetime64 or type(t1) == pd.Timestamp:
-        if 'datetime' in self.columns:
+        if "datetime" in self.columns:
             return self.loc[np.logical_and(self.datetime >= t1, self.datetime <= t2)]
-        elif 'datetime' not in self.columns:
+        elif "datetime" not in self.columns:
             return self.loc[np.logical_and(self.index >= t1, self.index <= t2)]
     elif np.logical_or(type(t1) == int, type(t1) == float):
-        if 'time' in self.columns:
+        if "time" in self.columns:
             return self.loc[np.logical_and(self.time >= t1, self.time <= t2)]
-        elif 'time' not in self.columns:
+        elif "time" not in self.columns:
             return self.loc[np.logical_and(self.index >= t1, self.index <= t2)]
     else:
-        print(f't1 and t2 must be strings, datetime64 or integers')
+        print(f"t1 and t2 must be strings, datetime64 or integers")
 
-#----------------------------------------- METHODS FOR ON-OFF DATAFRAMES ------------------------------------------------------
+
+# ----------------------------------------- METHODS FOR ON-OFF DATAFRAMES ------------------------------------------------------
+
 
 @pf.register_dataframe_method
 def oots(self, t1, t2):
@@ -75,27 +100,33 @@ def oots(self, t1, t2):
     if type(t1) == str:
         t1 = np.datetime64(t1)
         t2 = np.datetime64(t2)
-        if 'end_datetime' in self.columns:
-            return self.loc[np.logical_and(self.end_datetime >= t1, self.end_datetime <= t2)]
-        elif 'end_datetime' not in self.columns:
+        if "end_datetime" in self.columns:
+            return self.loc[
+                np.logical_and(self.end_datetime >= t1, self.end_datetime <= t2)
+            ]
+        elif "end_datetime" not in self.columns:
             return self.loc[np.logical_and(self.index >= t1, self.index <= t2)]
     elif type(t1) == np.datetime64 or type(t1) == pd.Timestamp:
-        if 'end_datetime' in self.columns:
-            return self.loc[np.logical_and(self.end_datetime >= t1, self.end_datetime <= t2)]
-        elif 'end_datetime' not in self.columns:
+        if "end_datetime" in self.columns:
+            return self.loc[
+                np.logical_and(self.end_datetime >= t1, self.end_datetime <= t2)
+            ]
+        elif "end_datetime" not in self.columns:
             return self.loc[np.logical_and(self.index >= t1, self.index <= t2)]
     elif np.logical_or(type(t1) == int, type(t1) == float):
-        if 'end_time' in self.columns:
+        if "end_time" in self.columns:
             return self.loc[np.logical_and(self.end_time >= t1, self.end_time <= t2)]
-        elif 'end_time' not in self.columns:
+        elif "end_time" not in self.columns:
             return self.loc[np.logical_and(self.index >= t1, self.index <= t2)]
     else:
-        print(f't1 and t2 must be strings, datetime64, integers, or floats')
+        print(f"t1 and t2 must be strings, datetime64, integers, or floats")
+
 
 @pf.register_dataframe_method
 def off_only(self):
-    return self.loc[self.state == 'off']
+    return self.loc[self.state == "off"]
+
 
 @pf.register_dataframe_method
 def on_only(self):
-    return self.loc[self.state == 'on']
+    return self.loc[self.state == "on"]
--- kdephys/units/pldf_methods.py
+++ kdephys/units/pldf_methods.py
@@ -1,20 +1,25 @@
 import polars as pl
 import numpy as np
 import pandas as pd
+import math
 import kdephys.unit_flavor as uf
+from acr.pl_units import bout_duration_similarity_check
 
 
 @uf.register_pldf_method
 def ch(self, chan: int):
     return self.filter(pl.col("channel") == chan)
 
+
 @uf.register_pldf_method
 def cid(self, cid: int):
     return self.filter(pl.col("cluster_id") == cid)
 
+
 @uf.register_pldf_method
 def cid_un(self):
-    return self['cluster_id'].unique()
+    return self["cluster_id"].unique()
+
 
 @uf.register_pldf_method
 def rec(self, rec):
@@ -23,17 +28,23 @@ def rec(self, rec):
     elif type(rec) == list:
         return self.filter(pl.col("recording").is_in(rec))
 
+
 @uf.register_pldf_method
 def prb(self, probe_store: str):
     return self.filter(pl.col("probe") == probe_store)
 
+
 @uf.register_pldf_method
 def pclus(self, probe_store: str, cluster_id: int):
-    return self.filter((pl.col("probe") == probe_store) & (pl.col("cluster_id") == cluster_id))
+    return self.filter(
+        (pl.col("probe") == probe_store) & (pl.col("cluster_id") == cluster_id)
+    )
+
 
 @uf.register_pldf_method
 def prbs(self):
-    return self['probe'].unique()
+    return self["probe"].unique()
+
 
 @uf.register_pldf_method
 def bl_rec(self):
@@ -42,14 +53,15 @@ def bl_rec(self):
     Returns:
         str or list: baseline recording name(s)
     """
-    recs = list(self['recording'].unique())
-    bl_rec = [rec for rec in recs if 'bl' in rec]
+    recs = list(self["recording"].unique())
+    bl_rec = [rec for rec in recs if "bl" in rec]
     if len(bl_rec) == 1:
         return bl_rec[0]
     elif len(bl_rec) > 1:
         return bl_rec
     else:
-        print('No baseline recording found')
+        print("No baseline recording found")
+
 
 @uf.register_pldf_method
 def ts(self, t1, t2):
@@ -62,17 +74,72 @@ def ts(self, t1, t2):
     if type(t1) == str:
         t1 = np.datetime64(t1)
         t2 = np.datetime64(t2)
-        assert 'datetime' in self.columns
-        return self.filter((pl.col('datetime') >= t1) & (pl.col('datetime') <= t2))
+        assert "datetime" in self.columns
+        return self.filter((pl.col("datetime") >= t1) & (pl.col("datetime") <= t2))
     elif type(t1) == np.datetime64 or type(t1) == pd.Timestamp:
-        assert 'datetime' in self.columns
+        assert "datetime" in self.columns
         if type(t1) == pd.Timestamp:
             t1 = np.datetime64(str(t1))
             t2 = np.datetime64(str(t2))
         assert type(t1) == np.datetime64
-        return self.filter((pl.col('datetime') >= t1) & (pl.col('datetime') <= t2))
+        return self.filter((pl.col("datetime") >= t1) & (pl.col("datetime") <= t2))
     elif np.logical_or(type(t1) == int, type(t1) == float):
-        assert 'time' in self.columns
-        return self.filter((pl.col('time') >= t1) & (pl.col('time') <= t2))
+        assert "time" in self.columns
+        return self.filter((pl.col("time") >= t1) & (pl.col("time") <= t2))
     else:
-        print(f't1 and t2 must be strings, datetime64, floats, or integers')
+        print(f"t1 and t2 must be strings, datetime64, floats, or integers")
+
+
+@uf.register_pldf_method
+def light_schedule(self):
+    """returns a list with the light schedule for the recording
+
+    Returns:
+        times: lsit with light schedule
+    """
+    start = self["datetime"].to_pandas().min()
+    end = self["datetime"].to_pandas().max()
+    chunks = (end - start).total_seconds() / 3600 / 12
+    chunks = math.ceil(chunks)  # round up to nearest integer
+    begin = pd.Timestamp(f'{start.date().strftime("%Y-%m-%d")} 09:00:00')
+    times = []
+    for i in np.arange(chunks + 1):
+        if i == 0:
+            times.append(begin)
+        else:
+            time = times[-1] + pd.Timedelta("12h")
+            times.append(time)
+    return times
+
+
+@uf.register_pldf_method
+def frate(self, by="cluster_id"):
+    """gets the firing rate over all bouts. Self should have columns 'probe', 'cluster_id', 'count', 'bout_duration'
+
+    Parameters
+    ----------
+    by : str, optional
+        get the firing rate by this parameter; DEFAULT = 'cluster_id'
+    """
+    assert "bout_duration" in self.columns
+    assert "count" in self.columns
+    if by == "cluster_id":
+        new = self.groupby(["probe", "cluster_id"]).sum()
+        return new.with_columns(
+            (pl.col("count") / pl.col("bout_duration")).alias("fr")
+        ).drop("count", "bout_duration")
+    elif by == "probe":
+        new = self.groupby(["probe", "cluster_id"]).sum()
+        new = bout_duration_similarity_check(new)
+        new = new.groupby(["probe", "bout_duration"]).sum()
+        return new.with_columns(
+            (pl.col("count") / pl.col("bout_duration")).alias("fr")
+        ).drop("count", "bout_duration")
+
+
+@uf.register_pldf_method
+def frates(self):
+    """simply turns count and bout_duration columns into a single 'fr' column, no grouping"""
+    return self.with_columns(
+        (pl.col("count") / pl.col("bout_duration")).alias("fr")
+    ).drop("count", "bout_duration")
