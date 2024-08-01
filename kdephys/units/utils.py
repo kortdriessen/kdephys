86cf340fb2b199ce87eec7b7acf0224fadb4ee16
Kort Driessene <driessenkort@gmail.com>
2024-08-01 17:23:20 -0500

    unit util updates

--- kdephys/units/utils.py
+++ kdephys/units/utils.py
@@ -1,6 +1,7 @@
 import yaml
 import pandas as pd
 import numpy as np
+from probeinterface import Probe
 
 
 def check_sorting_thresholds(threshes=[4, 10, 3]):
@@ -39,3 +40,33 @@ def check_recs_and_times(subject, sort_id):
         recs = ss_narrowed.recordings.values[0].split(",")
         recs = [r.strip() for r in recs]
     return times, recs
+
+
+def set_probe_and_channel_locations_on_si_rec(si_recording):
+    probe_spacing = 50
+
+    CONTACT_RADIUS = 7.5
+    POLYGON = [(-10, 50), (0, 0), (10, 50), (40, 850), (-40, 850)]
+
+    # create the probe object and set assign it to the concatenated recording
+    nchans = len(si_recording.get_channel_ids())
+    assert nchans == 16
+
+    # Contacts
+    positions = np.zeros((nchans, 2))
+    for i in range(nchans):
+        x = 0
+        y = (
+            nchans - (i + 1)
+        ) * probe_spacing + probe_spacing  # Invert here because channel 1 is most superficial and I'm getting lost with prb.set_contacts_ids() etc and don't want to invert stuff
+        positions[i] = x, y
+
+    prb = Probe(ndim=2, si_units="um")
+    prb.set_contacts(positions=positions, shapes="circle", shape_params={"radius": CONTACT_RADIUS})
+
+    # Geometry
+    prb.set_planar_contour(POLYGON)
+    prb.set_device_channel_indices(np.arange(nchans))
+
+    si_recording.set_probe(prb, in_place=True)
+    return si_recording
